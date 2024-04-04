e9985b57b9efd8b99ae4a156aed48ad7b7348f3f
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-04 12:31:00 +0530

    update

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -5,9 +5,18 @@ function WeatherDisplay({ weather }) {
   const temperatureStyle = {
     color: weather.temperature > 20 ? "red" : "blue",
   };
-
+  const temperatureStyleBG = {
+    backgroundColor: weather.temperature > 20 ? "red" : "blue",
+  };
   return (
     <div>
+      <span
+        style={{
+          backgroundColor: weather.temperature > 20 ? "red" : "blue",
+          width: "50px",
+          height: "50px",
+        }}
+      ></span>
       <p style={temperatureStyle}>Temperature: {weather.temperature}</p>
       <p>Conditions: {weather.conditions}</p>
     </div>
