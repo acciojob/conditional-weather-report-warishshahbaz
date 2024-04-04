26158d7fe53a81d892885671752e3ee0d5a008ff
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-04 12:38:12 +0530

    update

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,16 +3,16 @@ import React from "react";
 
 function WeatherDisplay({ weather }) {
   const temperatureStyle = {
-    color: weather.temperature > 20 ? "rgb(255, 0, 0)" : "blue",
+    color: weather.temperature > 20 ? rgb(255, 0, 0) : "blue",
   };
   const temperatureStyleBG = {
-    backgroundColor: weather.temperature > 20 ? "rgb(255, 0, 0)" : "blue",
+    backgroundColor: weather.temperature > 20 ? rgb(255, 0, 0) : "blue",
   };
   return (
     <div>
       <span
         style={{
-          backgroundColor: weather.temperature > 20 ? "rgb(255, 0, 0)" : "blue",
+          backgroundColor: weather.temperature > 20 ? rgb(255, 0, 0) : "blue",
           width: "50px",
           height: "50px",
         }}
